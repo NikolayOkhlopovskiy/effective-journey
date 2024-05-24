53f8e81a45164534c94759b1002cc0c236997752
NikolayOkhlopovskiy <154047449+NikolayOkhlopovskiy@users.noreply.github.com>
2024-05-24 12:47:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1 
+
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}+{y}={x + y}");
+            //Console.Read();
+
+            //Задание 2
+
+            //Console.Write("x=");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.Write("y=");
+            //int y = int.Parse(Console.ReadLine());
+            //Console.Write("z=");
+            //int z = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}+{y}+{z}={x + y + z}");
+            //Console.Read();
+
+            //Задание 3
+
+            //Console.Write("x");
+            //double x = double.Parse(Console.ReadLine());
+            //Console.Write("y");
+            //double y = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}*{y}={x * y:F1}");
+            //Console.Read();
+        }
+    }
+}
